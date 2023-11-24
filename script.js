44be9b669fb0390f40c0858b56f571db874c8236
justfumz <esthergee01@gmail.com>
2023-11-24 16:32:38 +0100

    uploading artGallery files

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeColor(name) {
+  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+  document.getElementById(name).style.backgroundColor = randomColor;
+}
